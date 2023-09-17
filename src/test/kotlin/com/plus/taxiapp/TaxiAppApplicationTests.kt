620b4aa4f1e9c84aeff1824f35a44d9a0ab9657f
sollertia <sollertia331@gmail.com>
2023-09-17 19:02:42 +0900

    init commit

---
+++ src/test/kotlin/com/plus/taxiapp/TaxiAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.plus.taxiapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaxiAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

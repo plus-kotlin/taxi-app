1499cb5e215dca74f8800ee408ffec786b6ec32d
chanhyeoKingOfDev <68278903+choichanhyeok@users.noreply.github.com>
2023-10-12 11:56:01 +0900

    Revert "Feature regist driver apply template"

--- src/main/kotlin/com/plus/taxiapp/taxi/domain/TaxiService.kt
+++
@@ -1,8 +0,0 @@
-package com.plus.taxiapp.taxi.domain
-
-import com.plus.taxiapp.taxi.domain.command.TaxiCommand
-import com.plus.taxiapp.taxi.domain.entity.TaxiInfo
-
-interface TaxiService {
-    fun driverRegister(register: TaxiCommand.Register): TaxiInfo
-}

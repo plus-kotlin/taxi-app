f0dc381712af403a39d6187d93f96d06fceb572f
hyeok <cksgurwkd12@naver.com>
2023-10-24 21:35:14 +0900

    ADD: api 계층의 컨트롤러까지 내려오는 로그 advice로 체크 후 로깅 기능 추가

---
+++ src/main/kotlin/com/plus/taxiapp/shared/advice/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,18 @@
+package com.plus.taxiapp.shared.advice.exception
+
+import com.plus.taxiapp.shared.exception.ErrorResponse
+import lombok.extern.slf4j.Slf4j
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@Slf4j
+@ControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(Exception::class)
+    fun handleAllExceptions(ex: Exception): ResponseEntity<ErrorResponse> {
+        val errorResponse = ErrorResponse("Exception000", ex.message ?: "An error occurred.")
+        //log.error("Exception: ", ex)
+        return ResponseEntity.internalServerError().body(errorResponse)
+    }
+}
